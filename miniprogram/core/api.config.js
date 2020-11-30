7f1bf7b91dfb56a97910af5eece80a2cd2394c9d
胡露 <hul@elimen.com.cn>
2020-11-30 17:45:25 +0800

    初始化nuxt项目

---
+++ miniprogram/core/api.config.js
@@ -0,0 +1,32 @@
+import http from '../utils/request.js';
+export default {
+  // 获取菜谱分类
+  getRecipeClassify: () => {
+    return http.get('jisuapi/recipe_class').then(resp => {
+      return resp.data;
+    });
+  },
+  // 获取对应分类下的数据
+  getListByClassid: (data) => {
+    return http.get('jisuapi/byclass', data).then(resp => {
+      return resp.data;
+    });
+  },
+  // 获取菜谱详情
+  getDetailByClassid: (id) => {
+    return http.get('jisuapi/detail', { id }).then(resp => {
+      return resp.data;
+    });
+  },
+  // 搜索菜谱
+  search: (data) => {
+    return http.get('jisuapi/search', data).then(resp => {
+      return resp.data;
+    }); 
+  },
+  findFood: (data) => {
+    return http.get('/JDAI/FoodApi', data).then(resp => {
+      return resp.data;
+    }); 
+  }
+}
---
+++ miniprogram/utils/request.js
@@ -0,0 +1,34 @@
+const baseUrl = 'https://way.jd.com/';
+const service =  function (method, url, params) {
+  const data = { ...params, appkey: '3fd0070c5fbd9d0ce290f741fcb6d5ed' };
+  return new Promise((reslove, reject) => {
+    wx.request({
+      url: baseUrl + url,
+      data,
+      method,
+      success: resp => {
+        if (resp.data.code !== '10000') {
+          console.log('request', resp.code);
+          uni.showToast({
+            title: resp.data.msg
+            // image: '../static/error.png'
+          });
+        } 
+        reslove(resp);
+      },
+      fail: error => {
+        resolve(error)
+      }
+    });
+  });
+}
+const http = {
+  get: function (url, data) {
+    return service('GET', url, data);
+  },
+  post: function (url, data) {
+    return service('POST', url, data);
+  },
+}
+
+export default http;
